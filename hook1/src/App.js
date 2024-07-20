c27c2f7a1bfb8c37e5c2a6a8e475c3c2fafb109d
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-07-20 21:48:30 +0900

    Feat :: useState #1
    useState hook에 중요성을 알아보기

--- hook1/src/App.js
+++ hook1/src/App.js
@@ -1,11 +1,52 @@
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [item, setItem] = useState(1);
+  // const item = useState(1)[0]; // 한개만 쓰고 싶을때 이렇게 사용하기
+  const incrementItem = () => setItem(item+1)
+  const decrementItem = () => setItem(item-1)
   return (
     <div className="App">
-      
+      <h1>Hello {item}</h1>
+      <button onClick={incrementItem}>더하기</button>
+      <button onClick={decrementItem}>빼기</button>
     </div>
   );
 }
 
 export default App;
+
+// Class 형식으로 useState 써보기(hook을 사용하지 않으면 이런식으로 바뀜)
+// class AppUgly extends React.Component{
+//   state = {
+//     item: 1
+//   }
+//   render() {
+//     const {item} = this.state
+//     return (
+//       <div className="App">
+//         <h1>Hello {item}</h1>
+//         <button onClick={this.incrementItem}>더하기</button>
+//         <button onClick={this.decrementItem}>빼기</button>
+//       </div>
+//     );
+//   }
+//   incrementItem = () => {
+//     this.setState(state => {
+//       return {
+//         item: state.item + 1
+//       }
+//     });
+//   }
+//   decrementItem = () => {
+//     this.setState(state => {
+//       return {
+//         item: state.item - 1
+//       }
+//     });
+//   }
+  
+// }
+
+// export default AppUgly
