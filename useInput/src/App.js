c7ff34c215439eeccec5ce8c39c8594f80d31f21
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-07-20 22:12:39 +0900

    Feat :: useInput 분리 이벤트 부르기 #3
    ...쓰면 분리된 이벤트 파일 부를때 개사기다

--- useInput/src/App.js
+++ useInput/src/App.js
@@ -1,8 +1,27 @@
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+// useInput 훅 정의
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    setValue(value)
+  };
+  return { value, onChange };
+};
+
+const App = () => {
+  const name = useInput("신민호");
+
   return (
     <div className="App">
+      <h1>useInput 사용하기</h1>
+          <input placeholder='이름을 입력해주세요' {...name}/> 
+          {/* 다르게 써보기 */}
+          {/* <input placeholder='이름을 입력해주세요' value={name.value} onChange={name.onChange}/> */} 
     </div>
   );
 }
