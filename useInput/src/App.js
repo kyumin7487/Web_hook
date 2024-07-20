b8da260460a4162a6baa776664f7e76fa1bc096d
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-07-20 22:22:46 +0900

    Feat :: 검증 기능 추가 #3

--- useInput/src/App.js
+++ useInput/src/App.js
@@ -2,20 +2,28 @@ import React, { useState } from 'react';
 import './App.css';
 
 // useInput 훅 정의
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
     const {
       target: { value }
     } = event;
-    setValue(value)
+    // hook과는 관계가 없지만 함수를 더 예쁘게 만들고 검증 기능이 가능하다
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value)
+    }
   };
   return { value, onChange };
 };
 
 const App = () => {
-  const name = useInput("신민호");
-
+  const maxLen = (value) => value.length < 10; // 10개 이상 못 입력하게 하기
+  // const maxLen = value => !value.include("@"); // 원하는 글자 막기
+  const name = useInput("신민호", maxLen);
   return (
     <div className="App">
       <h1>useInput 사용하기</h1>
