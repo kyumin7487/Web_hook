2c1f96987d487f224509482fcd478cd5e4006a99
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-07-20 22:59:55 +0900

    Feat :: useTabs #6
    render이 없다고 render이 안되는건 아니다

--- useTabs/src/App.js
+++ useTabs/src/App.js
@@ -1,9 +1,37 @@
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const content = [
+  {
+    tab: "1번",
+    content: "나는 신민호다"
+  },
+  {
+    tab: "2번",
+    content: "나는 김의현이다"
+  }
+]
+
+const useTabs = (initialTab, allTabs) => {
+  // allTabs가 배열이 아닐 경우 return
+  const isValidTabs = Array.isArray(allTabs);
+  const [currentIndex, setCurrentIndex] = useState(isValidTabs ? initialTab : 0);
+
+  return {
+    currentItem: isValidTabs ? allTabs[currentIndex] : null, // 배열이 아닐경우 null 주기
+    changeItem: setCurrentIndex
+  }
+}
+
+const App = () => {
+  const { currentItem, changeItem } = useTabs(0, content);
+  
   return (
     <div className="App">
-      
+      {content.map((section, index) => 
+        <button key={index} onClick={() => changeItem(index)}>{section.tab}</button>
+      )}
+      <div>{currentItem?.content}</div>
     </div>
   );
 }
