ce00618d058c5b221d8fd300d1f3fa2d0e8be0a3
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-07-23 21:12:32 +0900

    Feat :: 기본 useEffect 써보기 #8

--- useEffect/src/App.js
+++ useEffect/src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
+import React, { useState, useEffect} from "react";
 
 function App() {
+  const sayHello = () => {
+    console.log("안녕해준")
+  }
+  useEffect(() => {
+    sayHello();
+  })
   return (
     <div className="App">
     </div>
